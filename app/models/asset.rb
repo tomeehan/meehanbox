499395341eceeeaefba042b3e54307981885c47f
tomeehan <tom.meehan@me.com>
2017-02-12 16:35:30 +0000

    cleaned up url for uploaded files, added download route for assets

--- app/models/asset.rb
+++ app/models/asset.rb
@@ -3,10 +3,16 @@ class Asset < ApplicationRecord
 	belongs_to :user
 	  
 	#set up "uploaded_file" field as attached_file (using Paperclip) 
-	has_attached_file :uploaded_file
+	has_attached_file :uploaded_file, 
+               :url => "/assets/get/:id", 
+               :path => ":Rails_root/assets/:id/:basename.:extension"
 	  
 	validates_attachment_size :uploaded_file, :less_than => 10.megabytes   
 	validates_attachment_presence :uploaded_file
 	validates_attachment_content_type :uploaded_file, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
+
+	def file_name
+		uploaded_file_file_name
+	end
 
 end
