6a13eccd4b19c5791a989c31150a8091dc873c8b
tomeehan <tom.meehan@me.com>
2017-02-13 01:19:47 +0000

    added browse route

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'home#index'
   match 'assets/get/:id' => 'assets#get', :via => [:get], :as => 'download'
+  match "browse/:folder_id" => "home#browse", :as => "browse"
 
 end
