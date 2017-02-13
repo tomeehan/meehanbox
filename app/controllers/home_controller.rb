a34520adcb9aee621c18e70bbae7ef03bf7f4676
tomeehan <tom.meehan@me.com>
2017-02-13 01:23:10 +0000

    added browse method

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -7,4 +7,17 @@ class HomeController < ApplicationController
     	end
 	end
 	
+	def browse
+		@current_folder = current_user.folders.find(params[:folder_id])
+
+		if @current_folder
+			@folders = @current_folder.children
+			@assets = current_user.assets.order("uploaded_file_file_name desc") 
+			render :action => "index"
+		else
+			flash[:error] = "Whoa there! That's not your folder!"
+			redirect_to root_path
+		end
+	end
+
 end
