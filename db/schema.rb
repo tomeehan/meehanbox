ea7bd779081b17182e5cf051a5f59f5401174cc2
tomeehan <tom.meehan@me.com>
2017-02-13 01:25:53 +0000

    added new migration add_folder_id_to_assets

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170213003717) do
+ActiveRecord::Schema.define(version: 20170213012347) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 20170213003717) do
     t.string   "uploaded_file_content_type"
     t.integer  "uploaded_file_file_size"
     t.datetime "uploaded_file_updated_at"
+    t.integer  "folder_id"
   end
 
   create_table "folders", force: :cascade do |t|
